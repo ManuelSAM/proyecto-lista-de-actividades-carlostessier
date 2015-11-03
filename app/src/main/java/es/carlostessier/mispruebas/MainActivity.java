1a2dbf773b1d60a8848245424157c97010d56258
carlosfernandez <prof.carlos.tessier@gmail.com>
2015-11-03 17:24:54 +0100

    Initial commit

---
+++ app/src/main/java/es/carlostessier/mispruebas/MainActivity.java
@@ -0,0 +1,45 @@
+package es.carlostessier.mispruebas;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+
+    final static String TAG = MainActivity.class.getName();
+
+    String [] pruebas = {"pruebas1","prueba2","prueba3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(MainActivity.this,
+                        android.R.layout.simple_expandable_list_item_1,
+                        pruebas);
+
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String prueba = pruebas[position];
+        Log.d(TAG,"Pulsado "+prueba);
+
+        try{
+            Class<?> clase = Class.forName("es.carlostessier.mispruebas."+prueba);
+            Intent intent = new Intent(this,clase);
+            startActivity(intent);
+        }
+        catch(ClassNotFoundException e){
+            Log.d(TAG,"Error no encuentro la clase",e);
+        }
+    }
+}
---
+++ app/src/main/java/es/carlostessier/mispruebas/pruebas1.java
@@ -0,0 +1,16 @@
+package es.carlostessier.mispruebas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+
+public class pruebas1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pruebas1);
+
+
+    }
+}
